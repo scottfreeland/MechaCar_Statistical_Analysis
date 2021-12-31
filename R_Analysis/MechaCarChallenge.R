33cb467b4cedf06aff49e4d2ffffc01e5d825b0a
Scott Freeland <90366840+scottfreeland@users.noreply.github.com>
2021-12-30 22:11:10 -0500

    Added Resources folder with images for REAME.md, updated MechaCarChallenge.RScript.

--- R_Analysis/MechaCarChallenge.R
+++ R_Analysis/MechaCarChallenge.R
@@ -5,3 +5,10 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpgs))
 
+SuspenCoil <-  read.csv(file = 'Suspension_Coil.csv')
+
+total_summary <- SuspenCoil %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI)) 
+
+total_summary
+
+lot_summary <- SuspenCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI))
